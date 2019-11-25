fe74d22e6ccc43a4471c52c28e800efb491904d8
weeehabla <weeehaplay@gmail.com>
2019-11-25 10:00:50 +0100

    my answer

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,22 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+
+    var array = [];
+    var array1 = string.split('')
+    
+
+    for ( var i = 0 ; i < array1.length ; i++) {
+
+        for ( var j =i; j < array1.length ; j++ ) {
+
+            if (array1[i] !== array1[j]) {
+
+                array1.splice(i,1)
+
+            }
+        }
+    }
+
+return array1[0]
 };
