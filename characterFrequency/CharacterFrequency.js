ba539c0ac1103dbab4ebb065c1b3c33742fc98b8
weeehabla <weeehaplay@gmail.com>
2019-12-05 09:56:10 +0100

    my answer

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -1,6 +1,7 @@
 /*
 Character Frequency
-Write a function call characterFrequency that takes as its input a string and returns an array
+Write a function call characterFrequency 
+that takes as its input a string and returns an array
 of arrays as shown below sorted in descending order by frequency
 and then by ascending order by character.
 
@@ -11,3 +12,68 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+
+
+var characterFrequency = function(string){
+
+var obj = {}
+var array = []
+var string = string.split('')
+var string = string.sort()
+var hold 
+var arraySort 
+
+if (string !== ""){
+
+    for ( var i = 0 ; i <string.length ; i++){
+         if (obj[string[i]]){
+             obj[string[i]] = obj[string[i]] + 1
+            }else{
+                obj[string[i]] = 1
+            }
+    }
+
+     var keys = Object.keys(obj)
+
+    for ( var i = 0 ; i < keys.length ; i++){
+
+        array.push([keys[i],obj[keys[i]]])
+
+    }
+
+    // var bubbleSort = function(array){
+
+    //     if(array.length === 0 ){
+    //         return array
+    //     }
+
+    // for (var i = 0 ; i < array.length ; i++){
+
+    //     if(array[i+1] !== undefined){
+
+    //         if ( array[i][1] < array[i+1][1]){
+    //         hold = array[i][1]
+    //         array[i][1] = array[i+1][1]
+    //         array[i+1][1] = hold
+    //         }
+
+    //     }else{
+
+    //     }
+    //   }
+    // }
+    
+    // arraySort = bubbleSort(array)
+    
+
+    // return arraySort
+
+    return array
+
+    }else
+    {
+        return []
+    }
+
+}
