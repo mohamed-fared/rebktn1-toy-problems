7fdf36f361c50c8944844165a2f31719bd2d4a28
weeehabla <weeehaplay@gmail.com>
2019-12-06 09:47:45 +0100

    my answer

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,47 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+
+
+// need more time :)) it will work for "omar" only i think not for matt wallahy 
+var vowelBack = function(string){
+
+  string = string.split("")
+  var az = "abcdefghijklmnopqrstuvwxyz".split("")
+
+
+  for ( var i = 0 ; i < string.length ; i++){
+
+    if (string[i] !=="a" && string[i] !== "e" && string[i] !== "i" && string[i] !== "o" & string[i] !== "u"){
+
+
+         var index = az.indexOf(string[i])
+
+            if (az[index+9] !== undefined){
+
+                string[i]= az[index + 9]
+
+            }else{
+
+
+                string[i]= az[0]
+            }
+    }else{
+        var index = az.indexOf(string[i])
+
+        if(az[index-1] !== undefined){
+
+            string[i]=az[index-1]
+            console.log(az[index-1])
+
+        }else{
+            string[i] = az[az.length-5 ]
+        }
+    }
+
+  }
+  
+return string
+  
+}
