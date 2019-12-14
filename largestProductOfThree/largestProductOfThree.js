bc1fee2407dad0b6d3bff16dd25a61b427d9d04c
weeehabla <weeehaplay@gmail.com>
2019-12-14 09:14:11 +0100

    solved

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,25 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+function largestProductOfThree(array) {
+
+  var arr = []
+  var result = 1
+
+  for (var i = 0 ; i < array.length ;i++){
+
+    array[i] = Math.abs(array[i])
+
+  }
+  array = array.sort((a,b)=>{
+    return a-b 
+  })
+
+  var index = array.length
+  result = array[index-1]*array[index-2]*array[index-3]
+
+  return result
+}
+
+largestProductOfThree([1, 4, -5, -2])
+
